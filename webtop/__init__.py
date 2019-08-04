425e1612fb89224f0b2602d93acb5d901eb415b6
Evan Foster <evan.sterling.foster@gmail.com>
2019-08-04 16:37:43 -0600

    Intial commit, converted to asyncio

--- webtop/__init__.py
+++ webtop/__init__.py
@@ -1,51 +1,53 @@
 #!/usr/bin/env python3
 
-from collections import deque
-from requests_toolbelt.adapters import host_header_ssl  # type: ignore
-from threading import Lock, Thread, Event
-from time import sleep
-from typing import Dict, Collection, Optional, Callable, Any, Deque
-from yarl import URL
+import aiohttp
 import argparse
+import asyncio
+from collections import deque
 import datetime
 import json
 import math
 import os
-import requests
-import requests.compat
-import requests.adapters
 import signal
-import urllib3.poolmanager  # type: ignore
+import socket
+import time
+from threading import Event
+from typing import Dict, Collection, Optional, Deque, List, Any, Callable
 import yaml
+from yarl import URL
 
 
-class NoPoolManager(urllib3.poolmanager.PoolManager):
-    def connection_from_pool_key(self, pool_key, request_context=None):
-        scheme = request_context["scheme"]
-        host = request_context["host"]
-        port = request_context["port"]
-        return self._new_pool(scheme, host, port, request_context=request_context)
+class CustomResolver(aiohttp.resolver.AbstractResolver):
+    async def close(self) -> None:
+        await self.async_resolver.close()
 
+    def __init__(self, *args, custom_mappings: Optional[Dict[str, str]] = None, **kwargs):
+        super().__init__(*args, **kwargs)  # type: ignore
+        if custom_mappings is None:
+            self.custom_mappings: Dict[str, str] = {}
+        else:
+            self.custom_mappings = custom_mappings
+        self.async_resolver = aiohttp.resolver.AsyncResolver()  # type: ignore
 
-class NoPoolSSLAdapter(host_header_ssl.HostHeaderSSLAdapter):
-    def init_poolmanager(self, connections, maxsize, block=requests.adapters.DEFAULT_POOLBLOCK, **pool_kwargs):
-        self._pool_connections = connections
-        self._pool_maxsize = maxsize
-        self._pool_block = block
-
-        self.poolmanager = NoPoolManager(
-            num_pools=connections, maxsize=maxsize, block=block, strict=True, **pool_kwargs
-        )
-
-
-class NoPoolAdapter(requests.adapters.HTTPAdapter):
-    init_poolmanager = NoPoolSSLAdapter.init_poolmanager
+    async def resolve(self, host: str, port: int = 0, family: int = socket.AF_INET) -> List[Dict[str, Any]]:
+        if host in self.custom_mappings:
+            return [
+                {
+                    "hostname": host,
+                    "host": self.custom_mappings[host],
+                    "port": port,
+                    "family": family,
+                    "proto": 0,
+                    "flags": socket.AI_NUMERICHOST,
+                }
+            ]
+        return await self.async_resolver.resolve(host, port, family)
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("url", metavar="URL", type=str)
+    parser.add_argument("url", metavar="URL", type=URL)
 
     parser.add_argument(
         "--method",
@@ -56,7 +58,7 @@ def parse_args() -> argparse.Namespace:
         default="GET",
     )
 
-    parser.add_argument("-k", "--threads", metavar="N", type=int, help="Thread pool size", default=1)
+    parser.add_argument("-j", "--jobs", metavar="N", type=int, help="Number of jobs", default=1)
 
     parser.add_argument(
         "--request-history", metavar="N", type=int, help="Number of request results to track", default=1000
@@ -82,29 +84,29 @@ def parse_args() -> argparse.Namespace:
 def are_args_valid(args: argparse.Namespace) -> bool:
     return all(
         (
-            args.url.startswith(("http://", "https://")),
+            args.url.is_absolute(),
             args.request_history >= 1,
             args.timeout > 0,
-            args.threads > 0,
+            args.jobs > 0,
             args.resolve is None or ":" in args.resolve,
         )
     )
 
 
 class Result(object):
-    def __init__(self, *, response: Optional[requests.models.Response] = None, error: Optional[Exception] = None):
+    def __init__(self, *, response: Optional[aiohttp.ClientResponse] = None, error: Optional[Exception] = None):
         self.response = response
         self.error = error
-
         if self.response is None or self.error is not None:
             self.is_success = False
         else:
-            self.is_success = self.response.status_code >= 200 and self.response.status_code < 400
+            self.is_success = 200 <= self.response.status < 400
 
 
 class ResponseResult(Result):
-    def __init__(self, *, response: requests.models.Response):
+    def __init__(self, *, response: aiohttp.ClientResponse, start_time: float, end_time: float):
         super().__init__(response=response, error=None)
+        self.elapsed = datetime.timedelta(seconds=end_time - start_time)
 
 
 class ErrorResult(Result):
@@ -112,17 +114,18 @@ class ErrorResult(Result):
         super().__init__(response=None, error=error)
 
 
-def request(
-    *, url: URL, method: str, timeout: int, session: requests.sessions.Session, headers: Dict[str, str]
-) -> Result:
+async def request(*, url: URL, method: str, session: aiohttp.ClientSession) -> Result:
     try:
-        response = session.request(method, str(url), timeout=timeout, headers=headers)
-        return ResponseResult(response=response)
+        start_time = time.time()
+        async with session.request(method, url) as response:
+            await response.read()
+            end_time = time.time()
+            return ResponseResult(response=response, start_time=start_time, end_time=end_time)
     except Exception as e:
         return ErrorResult(error=e)
 
 
-def build_stats(*, url: str, method: str, results: Collection[Result]) -> dict:
+def build_stats(*, url: URL, method: str, results: Collection[Result]) -> dict:
     # no_ = Number Of
     no_results = len(results)
     no_successful_results = 0
@@ -137,11 +140,12 @@ def build_stats(*, url: str, method: str, results: Collection[Result]) -> dict:
         if isinstance(result, ResponseResult):
             assert result.response is not None
             no_responses += 1
-            sum_latency += math.ceil(result.response.elapsed / datetime.timedelta(milliseconds=1))
-            reason = f"HTTP {result.response.status_code}"
+            sum_latency += math.ceil(result.elapsed / datetime.timedelta(milliseconds=1))
+            reason = f"HTTP {result.response.status}"
         elif isinstance(result, ErrorResult):
             reason = str(type(result.error).__name__)
 
+        # noinspection PyUnboundLocalVariable
         reason_counts[reason] = reason_counts.get(reason, 0) + 1
 
     if no_results > 0:
@@ -155,7 +159,7 @@ def build_stats(*, url: str, method: str, results: Collection[Result]) -> dict:
         avg_latency = 0
 
     summary = {
-        "URL": url,
+        "URL": str(url),
         "Verb": method,
         "Sample Size": no_results,
         "Success Rate": f"{success_rate:3.9f}%",
@@ -171,75 +175,62 @@ def render_stats(stats: dict, _format: str) -> str:
         output = json.dumps(stats, indent=2)
     elif _format == "yaml":
         output = yaml.dump(stats, default_flow_style=False, sort_keys=False)  # type: ignore
+    # noinspection PyUnboundLocalVariable
     return output
 
 
-def main() -> None:
+async def main() -> None:
     args = parse_args()
     assert are_args_valid(args)
 
-    url = URL(args.url)
-    headers = {}
+    url: URL = args.url
+    custom_resolution = {}
+    resolver: Callable = aiohttp.resolver.DefaultResolver
     if args.resolve is not None:
-        parsed_url = URL(url)
         host, address = args.resolve.split(":")
-        if parsed_url.host == host:
-            url = url.with_host(address)
-            headers["Host"] = host
+        if url.host == host:
+            custom_resolution[host] = address
+
+            def resolver():
+                return CustomResolver(custom_mappings=custom_resolution)
 
     results: Deque[Result] = deque(maxlen=args.request_history)
-    results_lock = Lock()
     shutdown_event = Event()
 
-    def shutdown_signal_handler(signum, frame):
+    def shutdown_signal_handler(_, __):
         shutdown_event.set()
 
     for shutdown_signal in (signal.SIGINT, signal.SIGTERM):
         signal.signal(shutdown_signal, shutdown_signal_handler)
 
-    threads = []
+    tasks = []
 
-    def start_daemon(target: Callable[[], Any]) -> None:
-        # Fly, Pantalaimon!
-        thread = Thread(target=target)
-        threads.append(thread)
-        thread.daemon = True
-        thread.start()
-
-    def renderer() -> None:
+    async def renderer() -> None:
         while True:
             if shutdown_event.is_set():
                 return
 
-            with results_lock:
-                stats = build_stats(url=args.url, method=args.method, results=results)
+            stats = build_stats(url=url, method=args.method, results=results)
             output = render_stats(stats, _format=args.output_format)
 
             os.system("clear")
             print(output, flush=True)
 
-            sleep(0.1)
+            await asyncio.sleep(0.1)
 
-    start_daemon(target=renderer)
-    with requests.Session() as session:
-        session.mount("http://", NoPoolAdapter())
-        session.mount("https://", NoPoolSSLAdapter())
+    tasks.append(renderer())
+    timeout = aiohttp.ClientTimeout(connect=args.timeout)
+    connector = aiohttp.TCPConnector(force_close=True, limit=0, resolver=resolver())
+    async with aiohttp.ClientSession(timeout=timeout, connector=connector) as session:
 
-        def worker() -> None:
-            while True:
-                result = request(url=url, method=args.method, timeout=args.timeout, session=session, headers=headers)
-                with results_lock:
-                    results.append(result)
+        async def worker() -> None:
+            while not shutdown_event.is_set():
+                result = await request(url=url, method=args.method, session=session)
+                results.append(result)
 
-                if shutdown_event.is_set():
-                    return
-
-        for i in range(args.threads):
-            start_daemon(target=worker)
-
-        for thread in threads:
-            thread.join()
+        tasks.extend([worker() for _ in range(args.jobs)])
+        await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
