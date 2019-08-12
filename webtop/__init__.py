ec146daf96103ebb94eb58d7d577ee6f1cf2d46a
Evan Foster <evanfoster@users.noreply.github.com>
2019-08-12 14:39:04 -0600

    Convert webtop to asyncio (#5)

--- webtop/__init__.py
+++ webtop/__init__.py
@@ -1,23 +1,25 @@
 #!/usr/bin/env python3
 
-from collections import deque
-from threading import Lock, Thread, Event
-from time import sleep
-from typing import Dict, Collection, Optional, Callable, Any, Deque
+import aiohttp
 import argparse
+import asyncio
+from collections import deque
 import datetime
 import json
 import math
 import os
-import requests
 import signal
+import time
+from threading import Event
+from typing import Dict, Collection, Optional, Deque
 import yaml
+from yarl import URL
 
 
 def parse_args() -> argparse.Namespace:
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("url", metavar="URL", type=str)
+    parser.add_argument("url", metavar="URL", type=URL)
 
     parser.add_argument(
         "--method",
@@ -28,7 +30,7 @@ def parse_args() -> argparse.Namespace:
         default="GET",
     )
 
-    parser.add_argument("-k", "--threads", metavar="N", type=int, help="Thread pool size", default=1)
+    parser.add_argument("-k", "--workers", metavar="N", type=int, help="Number of workers", default=1)
 
     parser.add_argument(
         "--request-history", metavar="N", type=int, help="Number of request results to track", default=1000
@@ -50,25 +52,23 @@ def parse_args() -> argparse.Namespace:
 
 
 def are_args_valid(args: argparse.Namespace) -> bool:
-    return all(
-        (args.url.startswith(("http://", "https://")), args.request_history >= 1, args.timeout > 0, args.threads > 0)
-    )
+    return all((args.url.is_absolute(), args.request_history >= 1, args.timeout > 0, args.workers > 0))
 
 
 class Result(object):
-    def __init__(self, *, response: Optional[requests.models.Response] = None, error: Optional[Exception] = None):
+    def __init__(self, *, response: Optional[aiohttp.ClientResponse] = None, error: Optional[Exception] = None):
         self.response = response
         self.error = error
-
         if self.response is None or self.error is not None:
             self.is_success = False
         else:
-            self.is_success = self.response.status_code >= 200 and self.response.status_code < 400
+            self.is_success = 200 <= self.response.status < 400
 
 
 class ResponseResult(Result):
-    def __init__(self, *, response: requests.models.Response):
+    def __init__(self, *, response: aiohttp.ClientResponse, duration: datetime.timedelta):
         super().__init__(response=response, error=None)
+        self.elapsed = duration
 
 
 class ErrorResult(Result):
@@ -76,15 +76,19 @@ class ErrorResult(Result):
         super().__init__(response=None, error=error)
 
 
-def request(*, url: str, method: str, timeout: int) -> Result:
+async def request(*, url: URL, method: str, session: aiohttp.ClientSession) -> Result:
     try:
-        response = requests.request(method, url, timeout=timeout)
-        return ResponseResult(response=response)
+        start_time = time.time()
+        async with session.request(method, url) as response:
+            await response.read()
+            end_time = time.time()
+            duration = datetime.timedelta(seconds=end_time - start_time)
+            return ResponseResult(response=response, duration=duration)
     except Exception as e:
         return ErrorResult(error=e)
 
 
-def build_stats(*, url: str, method: str, results: Collection[Result]) -> dict:
+def build_stats(*, url: URL, method: str, results: Collection[Result]) -> dict:
     # no_ = Number Of
     no_results = len(results)
     no_successful_results = 0
@@ -99,8 +103,8 @@ def build_stats(*, url: str, method: str, results: Collection[Result]) -> dict:
         if isinstance(result, ResponseResult):
             assert result.response is not None
             no_responses += 1
-            sum_latency += math.ceil(result.response.elapsed / datetime.timedelta(milliseconds=1))
-            reason = f"HTTP {result.response.status_code}"
+            sum_latency += math.ceil(result.elapsed / datetime.timedelta(milliseconds=1))
+            reason = f"HTTP {result.response.status}"
         elif isinstance(result, ErrorResult):
             reason = str(type(result.error).__name__)
 
@@ -117,7 +121,7 @@ def build_stats(*, url: str, method: str, results: Collection[Result]) -> dict:
         avg_latency = 0
 
     summary = {
-        "URL": url,
+        "URL": str(url),
         "Verb": method,
         "Sample Size": no_results,
         "Success Rate": f"{success_rate:3.9f}%",
@@ -136,60 +140,48 @@ def render_stats(stats: dict, _format: str) -> str:
     return output
 
 
-def main() -> None:
+async def main() -> None:
     args = parse_args()
     assert are_args_valid(args)
 
     results: Deque[Result] = deque(maxlen=args.request_history)
-    results_lock = Lock()
     shutdown_event = Event()
 
-    def shutdown_signal_handler(signum, frame):
+    def shutdown_signal_handler(_, __):
         shutdown_event.set()
 
     for shutdown_signal in (signal.SIGINT, signal.SIGTERM):
         signal.signal(shutdown_signal, shutdown_signal_handler)
 
-    threads = []
+    tasks = []
 
-    def start_daemon(target: Callable[[], Any]) -> None:
-        # Fly, Pantalaimon!
-        thread = Thread(target=target)
-        threads.append(thread)
-        thread.daemon = True
-        thread.start()
-
-    def renderer() -> None:
+    async def renderer() -> None:
         while True:
             if shutdown_event.is_set():
                 return
 
-            with results_lock:
-                stats = build_stats(url=args.url, method=args.method, results=results)
+            stats = build_stats(url=args.url, method=args.method, results=results)
             output = render_stats(stats, _format=args.output_format)
 
             os.system("clear")
             print(output, flush=True)
 
-            sleep(0.1)
+            await asyncio.sleep(0.1)
 
-    start_daemon(target=renderer)
+    tasks.append(renderer())
+    timeout = aiohttp.ClientTimeout(connect=args.timeout)
+    connector = aiohttp.TCPConnector(force_close=True, limit=0)
+    async with aiohttp.ClientSession(timeout=timeout, connector=connector) as session:
 
-    def worker() -> None:
-        while True:
-            result = request(url=args.url, method=args.method, timeout=args.timeout)
-            with results_lock:
+        async def worker() -> None:
+            while not shutdown_event.is_set():
+                result = await request(url=args.url, method=args.method, session=session)
                 results.append(result)
 
-            if shutdown_event.is_set():
-                return
-
-    for i in range(args.threads):
-        start_daemon(target=worker)
-
-    for thread in threads:
-        thread.join()
+        for _ in range(args.workers):
+            tasks.append(worker())
+        await asyncio.gather(*tasks)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
